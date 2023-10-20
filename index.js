8ce5c4f727ba0513366a087bce9989ad5260cdc9
mkasiam <kawsarahmmed10001@gmail.com>
2023-10-20 09:54:10 +0600

    now cart items can be found by their id

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ async function run() {
       const result = await productCollection.findOne(query);
       res.send(result);
     });
+    app.get("/cart/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)};
+      const result = await cartCollection.findOne(query);
+      res.send(result);
+    })
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
       const result = await productCollection.insertOne(newProduct);
